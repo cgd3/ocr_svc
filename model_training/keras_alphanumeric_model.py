d82092ef42824cb8a0dab388caaef512fafd8fd6
David Shapiro <david.shapiro@tierpoint.com>
2018-06-08 12:19:01 -0400

    new sample method has been integrated into the model training

--- model_training/keras_alphanumeric_model.py
+++ model_training/keras_alphanumeric_model.py
@@ -1,120 +1,109 @@
-import numpy as np
-import cv2
-import random
-import os
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
+import os
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+import random
+import numpy as np
+import cv2
 
 
 letter_dir = 'base_letters/'
 img_size = 32
 batch = 32
 epoch = 50
+wiggle = 5
+base_size = 100
+min_font = 24
+max_font = 32
+samples_per_class = 32 * 4
+input_shape = (img_size, img_size, 1)
+font_dir = 'fonts/'
+save_dir = 'sample_letters/'
 charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 num_classes = len(charset)
-random.seed()
-sample_count = batch * 4000
-input_shape = (img_size, img_size, 1)
-wiggle = 5
 
 
 def add_gaussian_noise(image):
     gauss = np.random.normal(loc=0, scale=20, size=image.shape)
     gauss = gauss.reshape(image.shape)
     noisy = image + gauss
+    temp = noisy > 255
+    noisy[temp] = 255
+    temp = noisy < 0
+    noisy[temp] = 0
     return noisy
 
 
 def crop_image_random(image):
-    size = image.shape
-    x = round(size[0] / 2 - img_size / 2)
-    y = round(size[1] / 2 - img_size / 2)
-    x += random.randint(-wiggle, wiggle)  # add lateral variance
-    y += random.randint(0, wiggle*2)  # add vertical variance
+    x = round(base_size / 2 - img_size / 2)
+    y = round(base_size / 2 - img_size / 2)
+    y += random.randint(0, wiggle)  # add vertical variance
     return image[y:y + img_size, x:x + img_size]
 
 
-def random_brightness(image):
-    image += random.randint(-175, 75)
-    low_vals = image < 0
-    image[low_vals] = 0
-    high_vals = image > 255
-    image[high_vals] = 255
-    return image
+def make_random_image(font_file, c):
+    paper = random.randint(150, 255)
+    ink = random.randint(0, 75)
+    font_size = random.randint(min_font, max_font)
+    #underscore = bool(random.getrandbits(1))
+    # TODO add underscores to some images
+    font = ImageFont.truetype(font_dir + font_file, font_size)
+    left = random.choice(charset)
+    right = random.choice(charset)
+    string = left + c + right
+    canvas = Image.new('RGBA', (base_size, base_size), (paper, paper, paper))
+    draw = ImageDraw.Draw(canvas)
+    w, h = draw.textsize(text=string, font=font)
+    w = round((base_size - w) / 2)
+    h = round((base_size - h) / 2)
+    draw.text((w, h), string, (ink, ink, ink), font=font)
+    ImageDraw.Draw(canvas)
+    canvas = np.asarray(canvas)
+    canvas = cv2.cvtColor(canvas, cv2.COLOR_RGB2GRAY)
+    canvas = crop_image_random(canvas)
+    canvas = add_gaussian_noise(canvas)
+    return canvas
 
 
-def random_size(image):
-    factor = random.uniform(0.5, 1.5)
-    size = round(image.shape[0] * factor)
-    cv2.resize(image, dst=image, dsize=(size, size), fx=factor, fy=factor)
-    return image
-
-
-def randomize_image(image):
-    image = add_gaussian_noise(image)
-    image = random_brightness(image)
-    image = random_size(image)
-    image = crop_image_random(image)
-    return image
-
-
-def generate_samples(count):
-    # this can be accelerated by threading/multiprocessing
-    files = os.listdir(letter_dir)
-    secure_random = random.SystemRandom()
+def generate_samples():
+    random.SystemRandom()
+    random.seed()
+    fonts = os.listdir(font_dir)
     results = []
-    for i in range(count):
-        file = secure_random.choice(files)
-        img = cv2.imread(letter_dir + file)
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        img = randomize_image(img)
-        label = int(file.split('_')[1])
-        results.append((img, label))
-        if i % 500 == 0:
-            print('SAMPLES', i, 'OF', count)
+    for font in fonts:
+        for character in charset:
+            label = charset.index(character)
+            print('GENERATING SAMPLES - %s - %s' % (font, character))
+            for i in range(samples_per_class):
+                sample = make_random_image(font, character)
+                results.append((sample, label))
     return results
 
 
 def instantiate_model():
-    print('compiling model')
+    print('COMPILING MODEL')
     model = Sequential()
-
-    model.add(Convolution2D(filters=32,
-                            kernel_size=(3, 3),
-                            strides=(1, 1),
-                            activation='relu',
-                            input_shape=input_shape))
-
-    model.add(Convolution2D(filters=32,
-                            kernel_size=(3, 3),
-                            strides=(1, 1),
-                            activation='relu'))
-
+    model.add(Convolution2D(filters=32, kernel_size=(3, 3), strides=(1, 1), activation='relu', input_shape=input_shape))
+    model.add(Convolution2D(filters=32, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
     model.add(Flatten())
-    model.add(Dense(128,
-                    activation='relu'))
-
+    model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(num_classes,
-                    activation='softmax'))
-
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-
+    model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     print(model.summary())
     return model
 
 
 def prepare_datasets(samples):
     random.shuffle(samples)
-    training_set = samples[:round(sample_count / 10 * 9)]
-    test_set = samples[round(sample_count / 10 * 9):]
-
+    training_set = samples[:round(len(samples) / 10 * 9)]
+    test_set = samples[round(len(samples) / 10 * 9):]
     training_data = [i[0] for i in training_set]
     training_data = np.asarray(training_data).astype('float32')
     training_data = np.expand_dims(training_data, axis=3)
@@ -122,10 +111,8 @@ def prepare_datasets(samples):
     training_labels = [i[1] for i in training_set]
     training_labels = np.asarray(training_labels)
     training_labels = np_utils.to_categorical(training_labels, num_classes)
-
     print('TRAINING DATA SHAPE', training_data.shape)
     print('TRAINING LABELS SHAPE', training_labels.shape)
-
     test_data = [i[0] for i in test_set]
     test_data = np.asarray(test_data).astype('float32')
     test_data = np.expand_dims(test_data, axis=3)
@@ -133,14 +120,14 @@ def prepare_datasets(samples):
     test_labels = [i[1] for i in test_set]
     test_labels = np.asarray(test_labels)
     test_labels = np_utils.to_categorical(test_labels, num_classes)
-
     print('TEST DATA SHAPE', test_data.shape)
     print('TEST LABELS SHAPE', test_labels.shape)
     return training_data, training_labels, test_data, test_labels
 
 
 if __name__ == '__main__':
-    samples = generate_samples(count=sample_count)
+    samples = generate_samples()
+    print('SAMPLES GENERATED', len(samples))
     training_data, training_labels, test_data, test_labels = prepare_datasets(samples)
     classifier = instantiate_model()
     classifier.fit(training_data, training_labels, batch_size=batch, epochs=epoch, verbose=1)
